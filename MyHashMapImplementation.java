f6e50cb7394f9602d0c763c5fd6c3a13ddab48b1
pranav <prnvkatkar88@gmail.com>
2021-10-22 22:38:11 +0530

    Added:ability to find frequency of words in large paragraphs

--- MyHashMapImplementation.java
+++ MyHashMapImplementation.java
@@ -2,8 +2,8 @@ package hashmap;
 
 public class MyHashMapImplementation {
 	public static void main(String[] args) {
-		System.out.println("welcom to hash maps using linked list");
-		String sentence = "To be or not to be";
+		String sentence = "Paranoids are not paranoid because they are paranoid but because"
+				+ "they keep putting themselves deliberately into paranoid avoidable situations";
 		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
 		String[] words = sentence.toLowerCase().split(" ");
 		for (String word : words) {
@@ -14,8 +14,8 @@ public class MyHashMapImplementation {
 				value = value + 1;
 			myHashMap.add(word, value);
 		}
-		int frequency = myHashMap.get("be");
+		int frequency = myHashMap.get("paranoid");
 		System.out.println(myHashMap);
-		System.out.println("Frequency of word(to) is " + frequency);
+		System.out.println("Frequency of word (paranoid) is " + frequency);
 	}
 }
